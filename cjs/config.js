60dea50c64f5279208a66f8e78cc5944c6d892ca
austinhallock <austinhallock@gmail.com>
2020-07-17 22:23:48 -0500

    allow api url to be set with env

--- cjs/config.js
+++ cjs/config.js
@@ -4,7 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
+
+var _process, _process$env;
+
 var _default = {
-  API_URL: 'https://api.techby.org/impact/v1/graphql'
+  API_URL: ((_process = process) === null || _process === void 0 ? void 0 : (_process$env = _process.env) === null || _process$env === void 0 ? void 0 : _process$env.TECH_BY_IMPACT_API_URL) || 'https://api.techby.org/impact/v1/graphql'
 };
 exports["default"] = _default;
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  API_URL: 'https://api.techby.org/impact/v1/graphql'
+  API_URL: process?.env?.TECH_BY_IMPACT_API_URL || 'https://api.techby.org/impact/v1/graphql'
 }
